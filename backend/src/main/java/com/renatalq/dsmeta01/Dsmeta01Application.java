a05a2a7f4b6cc5fa2f9604868bf451b3330eda4f
Renata LQ <renatagouveiaq@gmail.com>
2022-09-07 12:57:15 -0300

    Project created

---
+++ backend/src/main/java/com/renatalq/dsmeta01/Dsmeta01Application.java
@@ -0,0 +1,13 @@
+package com.renatalq.dsmeta01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsmeta01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsmeta01Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/renatalq/dsmeta01/Dsmeta01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renatalq.dsmeta01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsmeta01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
